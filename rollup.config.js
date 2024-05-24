1eb9e68c9dbb9e68f09916adf4bcdc7b8888e38f
DanielRoesch <daniel.roesch@immersight.de>
2024-05-24 09:49:11 +0200

    add different typescript rollup plugin package and modify config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
+import typescript from "@rollup/plugin-typescript2";
 
 const packageJson = require("./package.json");
 
@@ -11,19 +11,22 @@ const rollupConfig = [
             {
                 file: packageJson.main,
                 format: "cjs",
-
+                sourceMap: true
             },
             {
                 file: packageJson.module,
                 format: "esm",
-
+                sourceMap: true
             },
         ],
         external: ['react', 'react-dom'],
         plugins: [
             resolve(),
             commonjs(),
-            typescript({tsconfig: "./tsconfig.json"}),
+            typescript({
+              useTsconfigDeclarationDir: true,
+              tsconfigOverride: { compilerOptions: { declaration: true, declarationDir: 'dist' } },
+            }),
         ],
     }
 ];
